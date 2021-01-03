7c3a987e5379c491b502cabfa79f11e8d0218ccb
Jia Hui Liou <30471836+jiahui816@users.noreply.github.com>
2021-01-03 20:28:01 +1100

    added css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./App.css";
+import { selectUser } from "./features/userSlice";
 import Feed from "./Feed";
 import Header from "./Header";
+
 import Sidebar from "./Sidebar";
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className="app">
       <Header />
@@ -12,9 +17,6 @@ function App() {
         <Sidebar />
         <Feed />
       </div>
-
-      {/* Feed */}
-      {/* Widgets */}
     </div>
   );
 }
